7d88188a84e7bb74ddff226db9c2f2faab4d996e
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-10-31 12:02:58 +0400

    написал тест для masks.py покрытие 100%

---
+++ tests/test_masks.py
@@ -0,0 +1,6 @@
+from src.masks import create_mask_card, create_mask_check
+
+
+def test_mask():
+    assert create_mask_card('1596837868705199') == '1596 83** **** 5199'
+    assert create_mask_check('11111111111111111111') == '**1111'
