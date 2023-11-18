81e59a09da9c513d519131db35ab6f29222a17c3
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-11-18 14:13:47 +0400

    исправил ошибки

---
+++ src/log.py
@@ -0,0 +1,13 @@
+import logging
+import utils
+
+file = utils
+def log_setup(__file__):
+    logging.basicConfig(level=logging.ERROR, filename="py_log.log",filemode="w", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.debug("A DEBUG Message")
+    logging.info("An INFO")
+    logging.warning("A WARNING")
+    logging.error("An ERROR")
+    logging.critical("A message of CRITICAL severity")
+    return logging.basicConfig
+
